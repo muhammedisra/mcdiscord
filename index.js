5b12d24d61af2fc730ae7f7c6ae484307c91e88c
muhammedisra <68696588+muhammedisra@users.noreply.github.com>
2020-07-25 11:22:04 +0530

    mc

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ bot.on('message', msg=>{
     if(msg.content === "Adharsh"){
         msg.reply('He is DEAD');
     }
+    if(msg.content === ":rofl:"){
+        msg.reply(':rofl:');
+    }
 })
 
 bot.login(process.env.token);
