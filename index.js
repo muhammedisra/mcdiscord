cfeba180ddff139077f23ca4a444b31ac9746d63
muhammedisra <68696588+muhammedisra@users.noreply.github.com>
2021-03-09 19:27:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ bot.on('ready', () =>{
 })
 
 bot.on('message', msg=>{
-    if(msg.content === shift().toUpperCase("HELLO")){
+    if(msg.content === toUpperCase("HELLO")){
         msg.reply('HELLO FRIEND');
     }
     if(msg.content === "Bye"){
