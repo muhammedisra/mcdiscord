fed8452ce1e11cb6f7df466a2faaa4f0ff5d6c6b
muhammedisra <68696588+muhammedisra@users.noreply.github.com>
2021-03-09 19:37:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ bot.on('ready', () =>{
 })
 
 bot.on('message', msg=>{
-    if(msg.content === args.shift().toLoweCase("hello")){
+    if(msg.content === "HELLO"){
         msg.reply('HELLO FRIEND');
     }
     if(msg.content === "Bye"){
