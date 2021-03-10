913aaa4640197b9738638c105fc1cb1e1def0482
muhammedisra <68696588+muhammedisra@users.noreply.github.com>
2021-03-10 09:12:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ bot.on('message', msg=>{
     }
     if (msg.content === "random") { // checks if the message says "?random"
         const number = Math.random()*10000; // generates a random number
-        let dat = Date.now;
+        const dat = new Date();
         msg.channel.send(dat);
         msg.channel.send(number.toString().substring(0,4)); // sends a message to the channel with the number
     }
