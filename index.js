aae20ac837364ae08608b8b1bd021167ddac2225
muhammedisra <68696588+muhammedisra@users.noreply.github.com>
2021-03-10 10:06:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ bot.on('message', msg=>{
     }
     if(msg.content === "time"){
         let date = new Date().toLocaleString('en-GB', {timeZone: 'Asia/Calcutta'});
-        msg.channel.send(date);
+        msg.channel.send(date.getDate());
     }
 
 })
