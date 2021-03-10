d1a123e302547075d6d23b0b57451b79daa80c3d
muhammedisra <68696588+muhammedisra@users.noreply.github.com>
2021-03-10 09:15:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ bot.on('message', msg=>{
         msg.reply('Isra is my master');
     }
     if (msg.content === "random") { // checks if the message says "?random"
+    let date = new Date();
+            // Send date
+            let content = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
+            msg.channel.send(content)
         const number = Math.random()*10000; // generates a random number
         msg.channel.send(number.toString().substring(0,4)); // sends a message to the channel with the number
     }
