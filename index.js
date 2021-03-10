9c7db25f5149214a69468276d25766f40e3849b8
muhammedisra <68696588+muhammedisra@users.noreply.github.com>
2021-03-10 08:38:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ bot.on('message', msg=>{
     if(msg.content === "Who is your master bot"){
         msg.reply('Isra is my master');
     }
+    if (message.content === "random") { // checks if the message says "?random"
+        const number = Math.random(); // generates a random number
+        message.channel.send(number.toString()); // sends a message to the channel with the number
+    }
 })
 
 bot.login(process.env.token);
