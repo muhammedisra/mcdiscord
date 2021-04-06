1b76e7cfafe33e101a8a105858948ae6c8500697
muhammedisra <68696588+muhammedisra@users.noreply.github.com>
2021-04-06 20:29:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 
 bot.on('ready', () =>{
     console.log('this bot is online');
-    bot.user.setActivity('Theory of relativity', { type: 'CUSTOM_STATUS'});
+    bot.user.setActivity('Theory of relativity', { type: 'STREAMING' , url: 'https//www.twitch.tv/pokimane' });
  })
 
 bot.on('message', msg=>{
