395f70230a394c262fa54c81653e98cc44a6907a
muhammedisra <68696588+muhammedisra@users.noreply.github.com>
2021-10-30 17:19:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -292,6 +292,8 @@ if(msg.content.toLowerCase().startsWith("bulkdelete")){
 bot.on("messageDelete", del =>{
     if(del.channel.id == "787302397902979073" && !del.author.bot)
     bot.channels.cache.get("851053934986395721").send(del.content+"\n Author "+del.author.username);
+    if(del.attachments.size != 0)
+    bot.channels.cache.get("851053934986395721").send(del.attachments.first().url);
   })
 
   bot.on("guildMemberUpdate", (om , nm ) =>{
